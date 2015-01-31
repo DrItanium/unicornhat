f5efeae577468291656adef348f66fc9270535e0
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-01-31 14:02:46 -0800

    Basic interface library implemented

--- unicornhat.go
+++ unicornhat.go
@@ -1,12 +1,15 @@
+// Interface functions with the pimoroni UnicornHat
 package unicornhat
 
-// #include "ws2812-RPi.h"
-// #include "unicornhat-bridge.h"
+//#include "ws2812-RPi.h"
+//
+//double getDefaultBrightnessMacro() {
+//	return DEFAULT_BRIGHTNESS;
+//}
 import "C"
-import "fmt"
 
-type Color struct {
-	r, g, b byte
+type Pixel struct {
+	R, G, B byte
 }
 
 // initialization of hardware
@@ -30,8 +33,8 @@ func Show() {
 func GetBrightness() float64 {
 	return float64(C.getBrightness())
 }
-func SetBrightness(brightness float64) byte {
-	return byte(C.setBrightness(C.double(brightness)))
+func SetBrightness(brightness float64) bool {
+	return (C.setBrightness(C.double(brightness)) == 1)
 }
 
 func ClearPWMBuffer() {
@@ -46,11 +49,46 @@ func ClearLEDBuffer() {
 	C.clearLEDBuffer()
 }
 
-func GetPixelColor(pixel uint) Color {
-	container := C.getPixelColor(C.uint(pixel))
-	return NewColor(byte(container.r), byte(container.g), byte(container.b))
+func GetPixelColor(pixel uint) Pixel {
+	return fromNativePixel(C.getPixelColor(C.uint(pixel)))
+}
+func NewPixel(r, g, b byte) Pixel {
+	return Pixel { R: r, G: g, B: b }
+	
+}
+func Color(r, g, b byte) Pixel {
+	return fromNativePixel(C.Color(C.uchar(r), C.uchar(g), C.uchar(b)))
+}
+func RGB2Color(r, g, b byte) Pixel {
+	return fromNativePixel(C.RGB2Color(C.uchar(r), C.uchar(g), C.uchar(b)))
 }
 
-func NewColor(r, g, b byte) Color {
-	return Color { r: r, g: g, b: b }
+func (this Pixel) nativePixel() C.Color_t {
+	var v C.Color_t
+	v.r = C.uchar(this.R)
+	v.g = C.uchar(this.G)
+	v.b = C.uchar(this.B)
+	return v
 }
+
+func fromNativePixel(pixel C.Color_t) Pixel {
+	return NewPixel(byte(pixel.r), byte(pixel.g), byte(pixel.b))
+}
+
+
+func NumPixels() uint {
+	return uint(C.numPixels())
+}
+
+func SetPixelColor(pixel uint, r, g, b byte) bool {
+	return (C.setPixelColor(C.uint(pixel), C.uchar(r), C.uchar(g), C.uchar(b)) == 1)
+}
+
+func SetPixelColorType(pixel uint, color Pixel) bool {
+	return (C.setPixelColorT(C.uint(pixel), color.nativePixel()) == 1)
+}
+
+func DefaultBrightness() float64 {
+	return float64(C.getDefaultBrightnessMacro())
+}
+
