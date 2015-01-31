2bd4561caa78107aad6b02b5cc9be72770e36185
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-01-31 13:36:19 -0800

    Added more interface functions

--- unicornhat.go
+++ unicornhat.go
@@ -5,7 +5,52 @@ package unicornhat
 import "C"
 import "fmt"
 
+type Color struct {
+	r, g, b byte
+}
+
+// initialization of hardware
 func InitHardware() {
 	C.initHardware()
 }
 
+func Initialize(numPixels int) {
+	C.init(C.int(numPixels))
+}
+
+func StartTransfer() {
+	C.startTransfer()
+}
+
+// Led updates
+func Show() {
+	C.show()
+}
+
+func GetBrightness() float64 {
+	return float64(C.getBrightness())
+}
+func SetBrightness(brightness float64) byte {
+	return byte(C.setBrightness(C.double(brightness)))
+}
+
+func ClearPWMBuffer() {
+	C.clearPWMBuffer()
+}
+
+func Clear() {
+	C.clear()
+}
+
+func ClearLEDBuffer() {
+	C.clearLEDBuffer()
+}
+
+func GetPixelColor(pixel uint) Color {
+	container := C.getPixelColor(C.uint(pixel))
+	return NewColor(byte(container.r), byte(container.g), byte(container.b))
+}
+
+func NewColor(r, g, b byte) Color {
+	return Color { r: r, g: g, b: b }
+}
