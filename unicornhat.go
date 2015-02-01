e10bb9e4a1548e743b3ffe94050464f17176e4ec
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-02-01 12:25:06 -0800

    Expanded feature set
    - The numLEDs field in C is now exposed through getters and setters
    - An Initialize function has been added which sets the number of pixels and
      brightness of the LEDs (use this above any other options)
    - Added a function to translate x and y coordinates to absolute pixel index. It
      was taken from unicorn.c in the pimoroni/UnicornHat github repo

--- unicornhat.go
+++ unicornhat.go
@@ -21,6 +21,12 @@ func Init(numPixels int) {
 	C.init(C.int(numPixels))
 }
 
+func Initialize(numPixels uint, brightness float64) {
+	SetNumLEDs(numPixels)
+	SetBrightness(brightness)
+	C.initHardware()
+}
+
 func StartTransfer() {
 	C.startTransfer()
 }
@@ -53,8 +59,8 @@ func GetPixelColor(pixel uint) Pixel {
 	return fromNativePixel(C.getPixelColor(C.uint(pixel)))
 }
 func NewPixel(r, g, b byte) Pixel {
-	return Pixel { R: r, G: g, B: b }
-	
+	return Pixel{R: r, G: g, B: b}
+
 }
 func Color(r, g, b byte) Pixel {
 	return fromNativePixel(C.Color(C.uchar(r), C.uchar(g), C.uchar(b)))
@@ -75,7 +81,6 @@ func fromNativePixel(pixel C.Color_t) Pixel {
 	return NewPixel(byte(pixel.r), byte(pixel.g), byte(pixel.b))
 }
 
-
 func NumPixels() uint {
 	return uint(C.numPixels())
 }
@@ -94,4 +99,32 @@ func DefaultBrightness() float64 {
 
 func Shutdown(dummy int) {
 	C.shutdown(C.int(dummy))
+}
+
+var coordinates [8][8]int = [8][8]int{
+	{7, 6, 5, 4, 3, 2, 1, 0},
+	{8, 9, 10, 11, 12, 13, 14, 15},
+	{23, 22, 21, 20, 19, 18, 17, 16},
+	{24, 25, 26, 27, 28, 29, 30, 31},
+	{39, 38, 37, 36, 35, 34, 33, 32},
+	{40, 41, 42, 43, 44, 45, 46, 47},
+	{55, 54, 53, 52, 51, 50, 49, 48},
+	{56, 57, 58, 59, 60, 61, 62, 63},
+}
+
+// taken from unicorn.c
+func CoordinateToPosition(x, y int) int {
+	return coordinates[x][y]
+}
+
+func SetNumLEDs(count uint) {
+	// since this is for the unicorn hat, I'm going to silently change this to 64
+	if count > 64 {
+		C.numLEDs = C.uint(64)
+	} else {
+		C.numLEDs = C.uint(count)
+	}
+}
+func GetNumLEDs() uint {
+	return uint(C.numLEDs)
 }
